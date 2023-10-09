b73979543b30a49ac2680cecb080669814ba0a20
dhanunjay <dhanunjayaburada6@gmail.com>
2023-10-09 22:12:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import hotBg from "./images/hot.jpg";
+import coldBg from "./images/cold.jpg";
+import Descriptions from "./components/Descriptions";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData } from "./weatherService";
+
+function App() {
+  const [city, setCity] = useState("Visakhapatnam");
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBg] = useState(hotBg);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const data = await getFormattedWeatherData(city, units);
+    
+        // Check if the data object is defined before updating the state
+        if (data) {
+          setWeather(data);
+    
+          // dynamic bg
+          const threshold = units === "metric" ? 20 : 60;
+          if (data.temp <= threshold) setBg(coldBg);
+          else setBg(hotBg);
+        } else {
+          
+          console.error("City not found.");
+        }
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [city,units]);
+
+ 
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.target.value);
+      e.target.blur();
+    }
+  };
+
+  return (
+    <div className="app" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="overlay">
+        {weather && (
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                onKeyDown={enterKeyPressed}
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+              />
+              {/* <button>°F</button> */}
+            </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>{`${weather.name}, ${weather.country}`}</h3>
+                <img src={weather.iconURL} alt="weatherIcon" />
+                <h3>{weather.description}</h3>
+              </div>
+              <div className="temperature">
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === "metric" ? "C" : "F"
+                }`}</h1>
+              </div>
+            </div>
+
+            {/* bottom description */}
+            <Descriptions weather={weather} units={units} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
